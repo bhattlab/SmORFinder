c3be062236350bcd90a663cfaad64a9a7ab27c8e
Matt George Durrant <mdurrant@smsh11dsu-srcf-d15-35.scg.stanford.edu>
2020-01-21 17:33:37 -0700

    preparing for setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="deepsmorfnet",
     version='0.0.2_dev',
-    py_modules=['deepsmorfnet'],
     description='A command line tool to identify and annotate small proteins in metagenomic sequencing datasets.',
     url='https://github.com/bhattlab/DeepSmORFNET',
     author="Matt Durrant",
